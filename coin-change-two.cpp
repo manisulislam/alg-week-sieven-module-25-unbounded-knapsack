8e57128467378abfe7cca223641ce97dec0f57c0
anisul islam <anisulislam980340@gmail.com>
2023-09-10 10:45:46 +0600

    coin change infinity update

--- coin-change-two.cpp
+++ coin-change-two.cpp
@@ -12,7 +12,7 @@ int main(){
 
     int dp[n+1][s+1];
     dp[0][0]= 0;
-    for(int i=1; i<= s; i++) dp[0][i]=INT_MAX;
+    for(int i=1; i<= s; i++) dp[0][i]=INT_MAX-1;
     for(int i=1; i<=n; i++){
         for(int j=0; j<= s; j++){
             if(w[i-1]<= j){
